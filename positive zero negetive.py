34a9ee0606f102e04bf8e5cd9b1b4436459ea0fd
bedabrata <bedabratap909@gmail.com>
2021-11-09 11:22:06 +0530

    Number Guess

---
+++ positive zero negetive.py
@@ -0,0 +1,14 @@
+def project():
+    while True:
+        num=int(input("Enter a number"))
+
+        if   num>0:
+            print(num,"This number is +ve")
+        elif num==0:
+            print(num,"This number is Zero")
+        else:
+            print(num,"This number is -ve")
+
+
+
+project()
